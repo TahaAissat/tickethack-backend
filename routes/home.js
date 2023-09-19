8860b1e698697e0cb4eb1b0f8f1a769fe777e55e
TahaAissat <tahayacineaissat@gmail.com>
2023-09-19 18:51:41 +0200

    recherche par date fix

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,15 @@
 var express=require('express');
 var router=express.Router()
 const Trip = require('../models/trips');
-const Cart = require('../models/cart')
+const Cart = require('../models/cart');
+const endOfDay=require('date-fns/endOfDay');
+const startOfDay=require('date-fns/startOfDay');
 
 
 // Route recherche dans collection trips à utiliser lors du click sur search de la page home.html
 router.get('/search/:departure/:arrival/:date', (req,res) => {
-    console.log(req.body);
-    Trip.find({departure : new RegExp(req.params.departure , 'i' ) , arrival : new RegExp(req.params.arrival, 'i'), date:new Date(req.params.date)})
+    Trip.find({departure : new RegExp(req.params.departure , 'i' ) , arrival : new RegExp(req.params.arrival, 'i'),
+     date:{$gte:startOfDay(new Date(req.params.date)),$lt:endOfDay(new Date(req.params.date))}})
     .then(trips => {
         if(!trips){
             res.json({result : false , message : 'No trips found'})
